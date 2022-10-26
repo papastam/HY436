850438b5fdf3141a1f97d149fd5043cbed9a32a9
Xristos Papastamos <csd4569@csd.uoc.gr>
2022-10-26 12:44:27 +0300

    added workspace launcher

---
+++ assignment1/workspace.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gnome-terminal -- sh -c "sshpass -p 'mininet' bash -c \"ssh -X -t mininet@192.168.56.101 'cd pox; bash --login'\""
+gnome-terminal -- sh -c "sshpass -p 'mininet' bash -c \"ssh -X -t mininet@192.168.56.101 'cd pox/pox; bash --login'\""
